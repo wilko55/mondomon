d074fa4b2ea88ec3feac8d42f67177ca37a5dc22
Andrew Wilkinson <andyjwilko@gmail.com>
2016-08-10 13:36:08 +0100

    Added body parser

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // daily max in pence
 var dailyMaxSpend = 1600
 
 app.post('/updateBalance', function (req, res){
-  console.log('>>>>')
   console.log(req.body)
-  console.log('>>>>')
-  console.log(req)
-  var currentDailySpend = 1500
+
+  var currentDailySpend = req.body.spend_today
   if (currentDailySpend >= (dailyMaxSpend - (dailyMaxSpend/10))){
+    console.log(currentDailySpend + ' spent today, close to daily max')
     res.send('close to daily max')
   }
-  if (currentDailySpend >= dailyMaxSpend){
+  else if (currentDailySpend >= dailyMaxSpend){
+    console.log(currentDailySpend + ' spent today, you hit daily max')
     res.send('You hit daily max: ' + currentDailySpend)
+  }
+  else {
+    console.log(currentDailySpend + ' spent today, you\'re all good')
+    res.send('You\'re fine, only spend ' + currentDailySpend + ' today :)')
   }
 })
 
