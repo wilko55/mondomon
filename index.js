2f59e9d6a732aa008e759e01362d0b6b0c38d9b9
Andrew Wilkinson <andyjwilko@gmail.com>
2016-08-10 11:16:39 +0100

    Testing webhook

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var app = express();
 var dailyMaxSpend = 1600
 
 app.post('/updateBalance', function (req, res){
+  console.log('>>>>')
+  console.log(req.body)
+  console.log('>>>>')
+  console.log(req)
   var currentDailySpend = 1500
   if (currentDailySpend >= (dailyMaxSpend - (dailyMaxSpend/10))){
     res.send('close to daily max')
@@ -13,7 +17,6 @@ app.post('/updateBalance', function (req, res){
     res.send('You hit daily max: ' + currentDailySpend)
   }
 })
-
 
 app.listen(5050, function(){
   console.log('Kicked off on 5050')
