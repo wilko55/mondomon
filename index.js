e821552f5cbf83bb4ed0ab16ccd3f85e738a0515
Andrew Wilkinson <andyjwilko@gmail.com>
2016-09-10 14:43:17 +0100

    Removed logging, refactored

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var http = require('http');
-
-var request = require('request');
+var User = require('./lib/User').User
 
 var key = process.env.IFTTT_KEY;
 var dailyMax = process.env.DAILY_MAX;
-var currentDailySpend = process.env.CURRENT_DAILY_SPEND
+var currentDailySpend = parseInt(process.env.CURRENT_DAILY_SPEND)
 
-var scheduledTasks = require('./lib/scheduledTasks').tasks(key, dailyMax)
+var user = new User(dailyMax, currentDailySpend)
+
+var scheduledTasks = require('./lib/scheduledTasks').tasks(key, user, dailyMax)
 app.use(bodyParser.json());
 
-require('./lib/routes').routes(app, request, key, dailyMax, currentDailySpend);
+require('./lib/routes')(app, user, key, dailyMax, currentDailySpend);
 
 app.listen(5050, function(){
   console.log('Kicked off on 5050. Daily max: ' + dailyMax + ' and currentDailySpend: ' + currentDailySpend)
